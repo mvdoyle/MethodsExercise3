e925a901b58c6e955c27f4eed863a8cfce947d0b
Michae Doyle <mdoyle@truecoders.io>
2020-09-03 16:05:15 -0500

    shell

---
+++ MethodsExercise3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MethodsExercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
